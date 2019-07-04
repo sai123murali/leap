ad7272f497a438cdac81c751ea70ffd4279c90f6
Epuru Sai Muralidhar <32213467+sai123murali@users.noreply.github.com>
2019-07-04 09:27:51 +0530

    Update week.py

--- week.py
+++ week.py
@@ -1,5 +1,5 @@
 n=input("")
-if((n=="Monday")or(n=="Tuesday")or(n=="Wednesday")or(n=="Thursday")or(n=="Saturday")):
+if((n=="Monday")or(n=="Tuesday")or(n=="Wednesday")or(n=="Thursday")or(n=="Friday")):
     print("no")
 elif((n=="Saturday")or(n=="Sunday")):
     print("yes")
