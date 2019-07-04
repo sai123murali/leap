d02923142de17c22732682e648b138869d5a0b82
Epuru Sai Muralidhar <32213467+sai123murali@users.noreply.github.com>
2019-07-04 09:23:46 +0530

    Create week.py

---
+++ week.py
@@ -0,0 +1,7 @@
+n=input("")
+if((n=="Monday")or(n=="Tuesday")or(n=="Wednesday")or(n=="Thursday")or(n=="Saturday")):
+    print("no")
+elif((n=="Saturday")or(n=="Sunday")):
+    print("yes")
+else:
+    print("no")
